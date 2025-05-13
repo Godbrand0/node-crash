bfb61d51ef04038cb004d3e3dc93957f7a44b2aa
godbrand <thompsoneregha005@gmail.com>
2025-05-13 10:26:54 +0100

    styles update

--- app.js
+++ app.js
@@ -2,23 +2,49 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
-app.get("/about");
 app.get("/", (req, res) => {
-  // res.send("<p>phone page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Youshi finds eggs",
+      snippet: "lorem hhiffdd dbfvdhfhs fdbfhdbfh fdhfh",
+    },
+    {
+      title: "mario finds star",
+      snippet: "lorem hhiffdd dbfvdhfhs fdbfhdbfh fdhfh",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "lorem hhiffdd dbfvdhfhs fdbfhdbfh fdhfh",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
+
+// app.get("/", (req, res) => {
+//   // res.send("<p>phone page</p>");
+//   // res.sendFile("./views/index.html", { root: __dirname });
+
+// });
 
 app.get("/about", (req, res) => {
   // res.send("<p>about page</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  // res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "about" });
 });
 
-app.get("/about-us", (req, res) => {
-  res, redirect("/about");
+// app.get("/about-us", (req, res) => {
+//   res, redirect("/about");
+// });
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "create" });
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  // res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
