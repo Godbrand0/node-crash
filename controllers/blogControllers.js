45346b2a8264c6b3ecd1b61c6807ba9f726da66a
godbrand <thompsoneregha005@gmail.com>
2025-05-26 02:08:59 +0100

    Update blog_details controller to render 404 page on error instead of logging error to console.

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -18,7 +18,7 @@ const blog_details = (req, res) => {
       res.render("details", { blog: result, title: "Blog Details" });
     })
     .catch((err) => {
-      console.log(err);
+      res.render("404", { title: "Page Not Found" });
     });
 };
 
